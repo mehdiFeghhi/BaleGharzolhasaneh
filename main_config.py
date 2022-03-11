fa70520358d76b7877b2ce0d2d1069b2b2ca3564
Mehdi <44398843+mehdiFeghhi@users.noreply.github.com>
2022-03-12 01:12:38 +0330

    delete bank card

--- main_config.py
+++ main_config.py
@@ -6,7 +6,7 @@ import os
 class BotConfig:
     base_file_url = os.environ.get('BASE_FILE_URL', "https://tapi.bale.ai/file/")
     base_url = os.environ.get('BASE_URL', "https://tapi.bale.ai/")
-    bank_card_number = int(os.environ.get('BANK_CARD_NUMBER', "6037997482019271"))
+    bank_card_number = int(os.environ.get('BANK_CARD_NUMBER', ""))
     web_hook_ip = os.getenv('WEB_HOOK_IP', "0.0.0.0")
     web_hook_port = int(os.getenv('WEB_HOOK_PORT', 9696))
     web_hook_domain = os.getenv('WEB_HOOK_DOMAIN', "https://testwebhook.bale.ai")
